1108be770b18894c7f714284050ceb13c7fc452a
Rejectt <45071435+Rejectt@users.noreply.github.com>
2018-11-28 10:28:20 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,9 +4,8 @@ const prefix = '!'
  
  
  
-client.on('ready', () => {
+client.on('ready', () => 
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`LastCodes4Ever  `,"https://www.twitch.tv/dggamingbot")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
